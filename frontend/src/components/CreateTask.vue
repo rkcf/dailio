2e742db3a4bc5b20d6f82dadf51c912324268ef3
rkcf <rkcf@rkcf.me>
2018-04-05 13:13:03 -0400

    add seperate component for task creation with basic POST to add task

---
+++ frontend/src/components/CreateTask.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Create Task</h2>
+    <input placeholder="Task Name" v-model="taskname">
+    <button v-on:click="addTask(taskname)">Create</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateTask',
+  data () {
+    return {
+      taskname: ''
+    }
+  },
+  methods: {
+    addTask: function (taskname) {
+      this.$http.post('/api/tasks/', {task_name: taskname})
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- frontend/src/components/ModalContainer.vue
+++ frontend/src/components/ModalContainer.vue
@@ -1,15 +1,25 @@
 <template>
   <diV id="modal-container">
     <div id="modal-content">
-      <h1>Modal Header</h1>
-      <button v-on:click="$emit('closeModal')">close</button>
+      <button id="close-modal" v-on:click="$emit('closeModal')">
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+          <path d="M0 0h24v24H0z" fill="none"/>
+        </svg>
+      </button>
+      <CreateTask/>
     </div>
   </div>
 </template>
 
 <script>
+import CreateTask from './CreateTask'
+
 export default {
-  name: 'ModalContainer'
+  name: 'ModalContainer',
+  components: {
+    CreateTask
+  }
 }
 </script>
 
@@ -28,5 +38,12 @@ export default {
   padding: 1rem;
   height: 300px;
   margin: 10rem auto;
+}
+
+#close-modal {
+  float: right;
+  fill: #000;
+  height: 1.5rem;
+  width: 1.5rem;
 }
 </style>
