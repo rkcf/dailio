1f212ab611682af78cf6ebcca4e309b3513c28bf
rkcf <rkcf@rkcf.me>
2018-04-06 15:57:30 -0400

    add fade transition to task cards

--- frontend/src/components/TaskCard.vue
+++ frontend/src/components/TaskCard.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="task-card">
-    <button class="delete-btn" v-on:click="deleteTask(task.task_id)">
-      <svg viewBox="0 0 24 24">
-        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-        <path d="M0 0h24v24H0z" fill="none"/>
-      </svg>
-    </button>
-    <h3>{{ task.task_name }}</h3>
-  </div>
+  <transition name="fade">
+    <div class="task-card">
+      <button class="delete-btn" v-on:click="deleteTask(task.task_id)">
+        <svg viewBox="0 0 24 24">
+          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+          <path d="M0 0h24v24H0z" fill="none"/>
+        </svg>
+      </button>
+      <h3>{{ task.task_name }}</h3>
+    </div>
+  </transition>
 </template>
 
 <script>
