5dab5dfa6192a447986130e33cf42533367448f5
rkcf <rkcf@rkcf.me>
2018-04-08 19:35:11 -0400

    add form validation to task name change

--- frontend/src/components/ModalTaskDetail.vue
+++ frontend/src/components/ModalTaskDetail.vue
@@ -1,16 +1,22 @@
 <template>
 <div v-if="task">
-  <div style="display: inline-block;" id="modal-header">
+  <div id="modal-header">
     <h2>
       {{ task.task_name }} <button id="edit-btn" v-on:click="editName = true">edit</button>
     </h2>
   </div>
   <div v-show="editName">
-    <form v-on:submit.prevent="changeTaskName(task.task_id, newName)">
-      <input name="newName"
-             v-model="newName"
+    <form v-on:submit.prevent="validateTaskName(task.task_id, newName)">
+        <input v-validate="'required|max:50'"
              placeholder="New Task Name?"
-      ><button class="btn">change</button>
+             v-model="newName"
+             name="newName"
+      ><button class="btn">Change</button>
+      <transition name="fade">
+        <div class="input-danger" v-show="errors.has('newName')">
+          {{ errors.first('newName') }}
+        </div>
+      </transition>
     </form>
     <button class="delete-btn" v-on:click="confirmDelete = true">
       delete
@@ -51,6 +57,14 @@ export default {
       // Change the name of the active task
       this.$store.dispatch('changeTaskName', { id, newName })
       this.editName = false
+    },
+    validateTaskName: function (id, newName) {
+      // Check if new task name is valid before changing
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.changeTaskName(id, newName)
+        }
+      })
     }
   }
 }
