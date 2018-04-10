2492e07624a43f13e0d484c15e91d09ed7c7c8cc
rkcf <rkcf@rkcf.me>
2018-04-10 17:25:20 -0400

    add task completion boolean field to model

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,3 +4,4 @@ class Task(models.Model):
     """Class for daily task"""
     task_id = models.AutoField(primary_key=True)
     task_name = models.CharField(max_length=50)
+    task_completed = models.BooleanField(default="False")
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,4 +5,4 @@ class TaskSerializer(serializers.ModelSerializer):
     """Class for daily task serialization into json for api"""
     class Meta:
         model = Task
-        fields = ('task_id', 'task_name')
+        fields = ('task_id', 'task_name', 'task_completed')
