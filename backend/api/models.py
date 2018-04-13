ca956e5f426913367be7189e5ad8eda43d3e0f5c
rkcf <rkcf@rkcf.me>
2018-04-13 15:57:17 -0400

    add commenting and checks for if the streak is at 0 to prevent going negative

--- backend/api/models.py
+++ backend/api/models.py
@@ -9,12 +9,16 @@ class Task(models.Model):
     task_maxstreak = models.IntegerField(default=0)
 
     def increment_streak(self):
+        """Increase the current task streak and set max streak if necessary"""
         self.task_streak += 1
         if self.task_maxstreak < self.task_streak:
             self.task_maxstreak = self.task_streak
         self.save()
 
     def decrement_streak(self):
+        """Decrease the current task streak and set max streak if necessary"""
+        if self.task_streak == 0:
+            return
         self.task_streak -= 1
         if self.task_maxstreak > self.task_streak:
             self.task_maxstreak = self.task_streak
