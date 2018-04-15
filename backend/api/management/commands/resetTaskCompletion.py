5dee2d8ec9c70f434ecf5e1bc997df506436959a
rkcf <rkcf@rkcf.me>
2018-04-15 12:35:18 -0400

    fix resetTaskCompletion django command to reset streak as well

--- backend/api/management/commands/resetTaskCompletion.py
+++ backend/api/management/commands/resetTaskCompletion.py
@@ -2,10 +2,12 @@ from django.core.management.base import BaseCommand
 from api.models import Task
 
 class Command(BaseCommand):
-    help = 'Set all tasks task_completion status to false'
+    help = 'Set all tasks task_completion status to false and reset streak to 0 if not completed'
 
     def handle(self, *args, **options):
         for task in Task.objects.all():
+            if task.task_completed == False:
+                task.task_streak = 0
             task.task_completed = False
             task.save()
 
