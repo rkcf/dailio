d61e684606aceaef5a96314ff5ab8ce3650cd8e0
rkcf <rkcf@rkcf.me>
2018-04-24 17:23:38 -0400

    fix linting in resetTaskCompletion managment command

--- backend/api/management/commands/resetTaskCompletion.py
+++ backend/api/management/commands/resetTaskCompletion.py
@@ -1,13 +1,13 @@
 from django.core.management.base import BaseCommand
 from api.models import Task
 
+
 class Command(BaseCommand):
-    help = 'Set all tasks task_completion status to false and reset streak to 0 if not completed'
+    help = 'Set all task completion to false and set streak to 0 if incomplete'
 
     def handle(self, *args, **options):
         for task in Task.objects.all():
-            if task.task_completed == False:
+            if not task.task_completed:
                 task.task_streak = 0
             task.task_completed = False
             task.save()
-
