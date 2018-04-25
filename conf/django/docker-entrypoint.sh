fa29f74e431d74f8564a497e05af7454eacc08b0
rkcf <rkcf@rkcf.me>
2018-04-25 19:12:32 -0400

    remove make migrations command from docker-entrypoing

--- conf/django/docker-entrypoint.sh
+++ conf/django/docker-entrypoint.sh
@@ -3,6 +3,5 @@
 python3 dailio/manage.py collectstatic --no-input
 
 #run migrations
-python3 dailio/manage.py makemigrations --no-input
 python3 dailio/manage.py migrate --no-input
 uwsgi --ini /var/www/uwsgi.ini
