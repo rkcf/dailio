d29a5ff0408a06c7d4e85927b3d326b5e8d2a5c9
rkcf <rkcf@rkcf.me>
2018-04-28 13:22:46 -0400

    add test for ModalContainer component

---
+++ frontend/test/unit/specs/ModalContainer.spec.js
@@ -0,0 +1,26 @@
+import { shallow, createLocalVue } from '@vue/test-utils'
+import ModalContainer from '@/components/ModalContainer'
+
+const localVue = createLocalVue()
+
+describe('ModalContainer.vue', () => {
+  let wrapper
+
+  beforeEach( () => {
+    wrapper = shallow(ModalContainer, { localVue })
+  })
+
+  describe('methods', () => {
+    describe('closeModal', () => {
+      it('should emit a close event on clicking close button', () => {
+        wrapper.find('#close-modal').trigger('click')
+        expect(wrapper.emitted().close).toBeTruthy()
+      })
+      it('should emit a close event on clicking outside modal', () => {
+        wrapper.find('#modal-container').trigger('click')
+        expect(wrapper.emitted().close).toBeTruthy()
+      })
+    })
+  })
+})
+
