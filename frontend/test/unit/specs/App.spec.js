886755e67f0cdc312ce877eb0a1b1e3604147256
rkcf <rkcf@rkcf.me>
2018-05-02 18:08:53 -0400

    add unit tests for App component

---
+++ frontend/test/unit/specs/App.spec.js
@@ -0,0 +1,76 @@
+import Vuex from 'vuex'
+import { shallow, createLocalVue } from '@vue/test-utils'
+import App from '@/App'
+// import sessionStorage from 'jest-localstorage-mock'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+describe('App.vue', () => {
+  let wrapper
+  let store
+  let actions
+  let mutations
+  let getters
+
+  beforeEach(() => {
+    actions = {
+      openModal: jest.fn()
+    }
+
+    getters = {
+      loggedIn: jest.fn(),
+      showModal: jest.fn()
+    }
+
+    mutations = {
+      setAuthToken: jest.fn()
+    }
+
+    store = new Vuex.Store({
+      actions,
+      mutations,
+      getters
+    })
+
+    wrapper = shallow(App,
+      { store,
+        localVue
+      })
+  })
+
+  describe('created', () => {
+    it('should dispaly header title', () => {
+      expect(wrapper.find('#title').isVisible()).toBeTruthy()
+    })
+
+// TODO test for token sessionStorage access
+//    it('should set store authToken if one is found in sessionStorage', async () => {
+//      sessionStorage.__STORE__ = {token: 'token'}
+//      expect(mutations.setAuthToken).toHaveBeenCalled()
+//    })
+
+    it('should call store loggedIn getter', () => {
+      expect(getters.loggedIn).toHaveBeenCalled()
+    })
+
+    it('should call store showModal getter', () => {
+      expect(getters.showModal).toHaveBeenCalled()
+    })
+  })
+
+  describe('methods', () => {
+    describe('toggleMenu', () => {
+      it('should show menu when the menu button is clicked', () => {
+        wrapper.find('#menu-btn').trigger('click')
+        expect(wrapper.vm.showMenu).toBeTruthy()
+      })
+      it('should hide menu when the menu button is clicked again', () => {
+        wrapper.find('#menu-btn').trigger('click')
+        wrapper.find('#menu-btn').trigger('click')
+        expect(wrapper.vm.showMenu).toBeFalsy()
+      })
+    })
+  })
+})
